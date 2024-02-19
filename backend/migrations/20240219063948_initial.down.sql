a29d846ba34c08a11ca18b70a300350502cf18d8
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2024-02-19 12:33:53 +0300

    Initial backend && db schema

---
+++ backend/migrations/20240219063948_initial.down.sql
@@ -0,0 +1,13 @@
+-- Add down migration script here
+
+DROP TABLE Consented;
+DROP TABLE Verdict;
+DROP TABLE Appointment;
+DROP TABLE Doctor;
+DROP TABLE Room;
+DROP TABLE Facility;
+DROP TABLE Speciality;
+DROP TABLE Patient;
+DROP TABLE Account;
+
+DROP TYPE ROLE;
---
+++ backend/migrations/20240219063948_initial.up.sql
@@ -0,0 +1,62 @@
+-- Add up migration script here
+
+CREATE TYPE ROLE AS ENUM('patient', 'doctor', 'admin');
+
+CREATE TABLE Account(
+    id SERIAL PRIMARY KEY,
+    role ROLE NOT NULL,
+    phone_number VARCHAR(32) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Patient(
+    id SERIAL PRIMARY KEY,
+    account_id INTEGER NOT NULL REFERENCES Account(id),
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    date_of_birth TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE Speciality(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE Facility(
+    id SERIAL PRIMARY KEY,
+    address VARCHAR(255)
+);
+
+CREATE TABLE Room(
+    id SERIAL PRIMARY KEY,
+    label VARCHAR(32),
+    facility_id INTEGER REFERENCES Facility(id) NOT NULL
+);
+
+CREATE TABLE Doctor(
+    id SERIAL PRIMARY KEY,
+    account_id INTEGER NOT NULL REFERENCES Account(id),
+    speciality_id INTEGER NOT NULL REFERENCES Speciality(id),
+    room_id INTEGER NOT NULL REFERENCES Room(id),
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE Appointment(
+    id SERIAL PRIMARY KEY,
+    doctor_id INTEGER NOT NULL REFERENCES Doctor(id),
+    patient_id INTEGER NOT NULL REFERENCES Patient(id),
+    time TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE Verdict(
+    id SERIAL PRIMARY KEY,
+    appointment_id INTEGER REFERENCES Appointment(id),
+    verdict TEXT
+);
+
+CREATE TABLE Consented(
+    doctor_id INTEGER NOT NULL REFERENCES Doctor(id),
+    verdict_id INTEGER NOT NULL REFERENCES Verdict(id)
+);
+
