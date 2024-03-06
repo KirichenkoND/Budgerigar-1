fe5da8db811e6dec6eac13642221b4d18c2316a0
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-06 17:03:14 +0300

    Add support image in navbar #2

--- frontend/src/components/HeaderLink/HeaderLink.tsx
+++ frontend/src/components/HeaderLink/HeaderLink.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import "./HeaderLink.scss"
 import { Link } from 'react-router-dom';
+
 interface HeaderLinkProps {
-    text: string;
+    image?: string;
+    text?: string;
     path: string;
 }
 
-const HeaderLink: React.FC<HeaderLinkProps> = ({ text, path }) => {
+const HeaderLink: React.FC<HeaderLinkProps> = ({ image, text, path }) => {
     return (
-        // <Link to={path}>
             <a href={path} className='header-link-route' >
                 <div className='header-link'>
+                    <img src={image}></img>
                     {text}
                 </div>
             </a>
-        // </Link>
     );
 };
 
