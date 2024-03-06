94a3e13adbdaa160d682c2ef5fd2069955a95aff
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-06 23:10:57 +0300

    Template for mainpage

--- frontend/src/Pages/MainPage/MainPage.tsx
+++ frontend/src/Pages/MainPage/MainPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import MainPageCard from '../../components/MainPageCard/MainPageCard';
 
 
 const MainPage = () => {
     return (
         <div className='main-title'>
             <h2>Новости</h2>
-
+            <MainPageCard />
         </div>
     );
 };
---
+++ frontend/src/components/MainPageCard/MainPageCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './MainPageCard.scss';
+
+
+const MainPageCard: React.FC = ({})  => {
+    return (
+        <>
+            <div className='MainPageCard'>
+                <div className='test'>
+                    12345
+                </div>
+                <div className='test'>
+                    12345
+                </div>
+                <div className='test'>
+                    12345
+                </div>
+            </div>
+        </>
+    );
+}
+
+
+export default MainPageCard;
