c292789a3ad496f017ee01093137aaae2c1acdac
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-06 17:02:25 +0300

    Add panel to appointment #11 #13

--- frontend/src/components/PatientList/PatientCard.tsx
+++ frontend/src/components/PatientList/PatientCard.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PatientCard.scss';
+import Button from '../../UI/Button/Button';
+import Input from "../../UI/Input/Input";
+import Popup from '../Popup/Popup';
+
+export interface Sessions {
+    visitdate: string;
+    report: string;
+    diagnosis: string;
+    recommendations: string;
+}
 
 interface PatientCardProps {
     id: number;
@@ -8,12 +18,19 @@ interface PatientCardProps {
     gender: string;
     age: number;
     insuranceNumber: string;
-    lastvisitdate: string;
-    report: string;
-    diagnosis: string
+    sessions: Sessions[];
 }
 
-const PatientCard: React.FC<PatientCardProps> = ({ id, name, address, gender, age, insuranceNumber, lastvisitdate, report, diagnosis }) => {
+const PatientCard: React.FC<PatientCardProps> = ({ id, name, address, gender, age, insuranceNumber, sessions }) => {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const openPopup = () => {
+        setIsPopupOpen(true);
+    };
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    };
+
     return (
         <div className="patient-card">
             <div>
@@ -25,11 +42,36 @@ const PatientCard: React.FC<PatientCardProps> = ({ id, name, address, gender, ag
                 <p>Адрес: {address}</p>
                 <p>Номер договора: {insuranceNumber}</p>
             </div>
-            <div>
-                <p>Дата посещения: {lastvisitdate}</p>
-                <p>Жалобы: {report}</p>
-                <p>Диагноз: {diagnosis}</p>
+            <p>Визиты к врачу:</p>
+
+            <div className="scroll-list">
+                <div className="sessions-list">
+                    {sessions.slice().reverse().map((session, index) => (
+                        <div key={index} className="session-item">
+                            <p><strong>Дата визита:</strong> {session.visitdate}</p>
+                            <p><strong>Жалобы:</strong> {session.report}</p>
+                            <p><strong>Диагноз:</strong> {session.diagnosis}</p>
+                            <p><strong>Рекомендации:</strong> {session.recommendations}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
+
+            <div>
+                <Button text="Новый приём" onClick={openPopup} />
+            </div>
+
+            <Popup isOpen={isPopupOpen} onClose={closePopup}>
+                <h3>Новый приём <br/>{name}</h3>
+                <div>
+                    <span className="user-name"></span>
+                    <p>Жалобы: <Input /></p>
+                    <p>Диагноз: <Input /></p>
+                    <p>Рекомендации: <Input /></p>
+                </div>
+                <Button text="Завершить приём" onClick={closePopup} />
+            </Popup>
+
         </div>
     );
 }
