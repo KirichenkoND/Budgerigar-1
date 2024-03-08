d9be9db0d21749a359003cc2beaadbd9e433febe
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-09 02:02:57 +0300

    Update medicalCardPatient #14

--- frontend/src/components/MePage/MePage.tsx
+++ frontend/src/components/MePage/MePage.tsx
@@ -1,31 +1,67 @@
 import React from 'react';
 import './MePage.scss';
+import useravatar from '../../../public/user.svg';
+import { MiniCards } from '../../UI/MiniCards/MiniCards';
 
-const MePage = () => {
+const MePage: React.FC = () => {
     return (
         <>
             <div className='MeContainer'>
                 <div className='MeData'>
                     <div className='MeAside'>
-                        <span>Фамилия</span>
-                        <span>Пиванов</span>
-                        <span>Имя</span>
-                        <span>Пиван</span>
-                        <span>Отчество</span>
-                        <span>Пиванович</span>
-                        <span>Возраст</span>
-                        <span>24</span>
-                        <span>Договор</span>
-                        <span>ABCDE001</span>
+                        <div className='MeAside2'>
+                            <button type="button" className="profileButton">
+                                <img src={useravatar} />
+                            </button>
+                            <span>Фамилия</span>
+                            <span>Пиванов</span>
+                            <span>Имя</span>
+                            <span>Пиван</span>
+                            <span>Отчество</span>
+                            <span>Пиванович</span>
+                            <span>Возраст</span>
+                            <span>24</span>
+                            <span>Договор</span>
+                            <span>ABCDE001</span>
+                        </div>
                     </div>
                 </div>
 
                 <div className='MeInfoContainer'>
                     <div className='MeInfoWrapper'>
-                        <span>12345</span>
+                        <span>Адрес проживания</span>
+                        <span>Адрес: 362300, Липецкая область, город Балашиха, шоссе Ладыгина, 02</span>
                     </div>
                     <div className='MeInfoWrapper'>
-                        <span>1078901</span>
+                        <span>Визиты к врачу</span>
+                        <div className="scroll-list">
+                            <div className="sessions-list">
+                                <div className="session-item">
+                                    <p><strong>Дата визита:</strong></p>
+                                    <p><strong>Жалобы:</strong></p>
+                                    <p><strong>Диагноз:</strong></p>
+                                    <p><strong>Рекомендации:</strong></p>
+                                </div>
+                                <div className="session-item">
+                                    <p><strong>Дата визита:</strong></p>
+                                    <p><strong>Жалобы:</strong></p>
+                                    <p><strong>Диагноз:</strong></p>
+                                    <p><strong>Рекомендации:</strong></p>
+                                </div>
+                                <div className="session-item">
+                                    <p><strong>Дата визита:</strong></p>
+                                    <p><strong>Жалобы:</strong></p>
+                                    <p><strong>Диагноз:</strong></p>
+                                    <p><strong>Рекомендации:</strong></p>
+                                </div>
+                                <div className="session-item">
+                                    <p><strong>Дата визита:</strong></p>
+                                    <p><strong>Жалобы:</strong></p>
+                                    <p><strong>Диагноз:</strong></p>
+                                    <p><strong>Рекомендации:</strong></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
